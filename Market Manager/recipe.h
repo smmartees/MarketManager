a496afceaa01130116bca104cb3ea32fda2846c5
SAM PELOT <sampelot93@gmail.com>
2024-12-22 23:24:07 -0500

    Sam Commit #1 - started setting up header files

---
+++ Market Manager/recipe.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//interface for recipe
+
+#include "ingredients.h"
+#include "instructions.h"
+#include <stdio.h>
+
+#define MAX_NAME	100
+
+typedef struct recipe {
+	char recipeName[MAX_NAME];
+	// ingredients
+	// instructions
+}RECIPE, *PRECIPE;
+
+// C
+
+
+// R
+
+
+// U
+
+
+// D
--- Market Manager/recipebook.h
+++ Market Manager/recipebook.h
@@ -2,3 +2,25 @@
 
 
 //interface for recipebook
+
+#include "recipe.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+
+typedef struct recipebook {
+	RECIPE data;
+	struct recipebook* next;
+}RECIPEBOOK, *PRECIPEBOOK;
+
+
+// C
+bool AddRecipeToBook(RECIPE Recipe, PRECIPEBOOK* RecipeBook);
+
+// R
+
+
+// U
+
+
+// D
